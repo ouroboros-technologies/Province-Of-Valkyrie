12a48d35858120b9f3141834cd6b4be5bbda1956
Justin Baker <33433146+justinwbaker@users.noreply.github.com>
2019-06-27 02:35:10 -0300

    got rid of debug and compiled server

--- Assets/Oroboros/Scripts/Entity/Item/weapon/Melee/RayCastMelee.cs
+++ Assets/Oroboros/Scripts/Entity/Item/weapon/Melee/RayCastMelee.cs
@@ -38,7 +38,6 @@ public class RayCastMelee : MonoBehaviour
         position = transform.position;
         CalculateVelocity(Time.deltaTime);
         CalculateForce();
-        Debug.Log(isEnabled);
         if (isEnabled)
         {
             Ray ray = new Ray(previousPosition, position - previousPosition);
